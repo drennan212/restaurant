8028b78069e3f44f9a2078585e16a6afe57030a1
drennan212 <58539335+drennan212@users.noreply.github.com>
2022-07-06 13:58:31 +0300

    Update [id].jsx

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -92,7 +92,7 @@ const Product = ({pizza}) => {
 }
 
 export const getServerSideProps = async ({params}) =>{
-    const res = await axios.get(`http://localhost:3000/api/products/${params.id}`);
+    const res = await axios.get(`https://restaurant-psi-ten.vercel.app/api/products/${params.id}`);
     return {
       props: {
         pizza: res.data,
